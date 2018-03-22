3f623fb5ab78e1f54e79828ba6e23cee2f82543d
haru067 <haru.pdf@gmail.com>
2018-03-23 08:29:30 +0900

    Fix typo

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ tmpfile=$(mktemp)
 python format.py ./src/haru.yml $tmpfile
 mustache $tmpfile ./template/haru.html > ./dist/web/haru.html
 mustache $tmpfile ./template/haru.itermcolors > ./dist/iterm/haru.itermcolors
-mustache $tmpfile ./template/haru067-color-themes.json > ./dist/vscode/themes/haru067-color-themes.json
+mustache $tmpfile ./template/haru067-color-theme.json > ./dist/vscode/themes/haru067-color-theme.json
 rm $tmpfile
