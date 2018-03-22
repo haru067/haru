fda1974bf1c0c58546f763d4aec319d568fb37f3
haru067 <haru.pdf@gmail.com>
2018-03-23 07:05:44 +0900

    Add itermcolors

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 tmpfile=$(mktemp)
 python format.py ./src/haru.yml $tmpfile
 mustache $tmpfile ./template/haru.html > ./dist/haru.html
+mustache $tmpfile ./template/haru.itermcolors > ./dist/haru2.itermcolors
 rm $tmpfile
