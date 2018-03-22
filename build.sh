bf0ba4976cdfe98dea44279e956832ec893f3f9b
haru067 <haru.pdf@gmail.com>
2018-03-23 08:17:32 +0900

    Rename

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 tmpfile=$(mktemp)
 python format.py ./src/haru.yml $tmpfile
 mustache $tmpfile ./template/haru.html > ./dist/web/haru.html
-mustache $tmpfile ./template/haru.itermcolors > ./dist/iterm/haru2.itermcolors
+mustache $tmpfile ./template/haru.itermcolors > ./dist/iterm/haru.itermcolors
 rm $tmpfile
