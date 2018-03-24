49cae415986786fb73aa3a0c822e41e0175c20cd
haru067 <haru.pdf@gmail.com>
2018-03-24 16:57:04 +0900

    Add template-generator

---
+++ template-generator.py
@@ -0,0 +1,33 @@
+import re
+import sys
+import yaml
+import subprocess
+from functools import reduce
+
+def hex_to_int_color(v):
+    if v[0] == '#':
+        v = v[1:]
+    return int(v[:2], 16), int(v[2:4], 16), int(v[4:6], 16)
+
+def is_hex(v):
+    if len(v) != 6:
+        return False
+    pattern = r"[0-9a-fA-F]+"
+    return re.match(pattern, v)
+
+if len(sys.argv) != 3:
+    print('Usage: python ./replace.py [colorfile] [input]')
+    sys.exit()
+f = open(sys.argv[1], 'r+')
+colors = yaml.load(f)
+f.close()
+
+cmd = ['perl', '-p', '-e']
+op = ""
+for k in colors:
+    if (not is_hex(colors[k])):
+        continue
+    op += f's/{colors[k]}/{{{{ {k} }}}}/gi;'
+cmd.append(op) 
+cmd.append(sys.argv[2])
+subprocess.call(cmd)
